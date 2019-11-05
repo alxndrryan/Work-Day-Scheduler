4bf1f710e7b5b80df667e01ecf11b201020b7efa
Rand al'Thor <alexanderhogle@gmail.com>
2019-11-05 00:58:56 -0600

    adds the rows labeled by time

---
+++ script.js
@@ -0,0 +1,12 @@
+// var newDiv = $("<div>");
+
+// newDiv.text("A pleasure to meet you!");
+
+
+// $("#empty-div").append(newDiv);
+
+var balls = $("<div>");
+
+var nineAm = $("<div>").text("9AM");
+
+$(balls).append(nineAm);
