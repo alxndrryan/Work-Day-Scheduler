61b0050f7c9fb10740288671ef891de22d337c5c
Rand al'Thor <alexanderhogle@gmail.com>
2019-11-06 16:20:42 -0600

    adds second html for testing

--- script.js
+++ script.js
@@ -2,16 +2,6 @@
 
 // newDiv.text("A pleasure to meet you!");
 
-
-// $("#empty-div").append(newDiv);
-
-var balls = $("<div>");
-
-var nineAm = $("<div>").text("9AM");
-
-$(balls).append(nineAm);
-
-
 var nineAm = document.querySelector("#nineAm");
 
 var todos = [];
