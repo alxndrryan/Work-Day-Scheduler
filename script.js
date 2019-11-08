b0708e376156ecc1beaea32e0d782f7e22982a6e
Rand al'Thor <alexanderhogle@gmail.com>
2019-11-07 18:01:12 -0600

    text displays in local storage

--- script.js
+++ script.js
@@ -3,7 +3,18 @@
 // newDiv.text("A pleasure to meet you!");
 
 var nineAm = document.querySelector("#nineAm");
+var submitButton = document.querySelector("#submit");
 
-var todos = [];
+var todos = localStorage.getItem("todos");
+
+todos = nineAm.value;
+
+submitButton.addEventListener("click", function(){
+    todos = nineAm.value;
+
+    localStorage.setItem("todos", todos);
+})
+
+
 
 //when button clicked, save text
